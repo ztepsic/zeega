b0e323387007c88b1883ad7dc08b62097fbc4318
Zeljko Tepsic <ztepsic@gmail.com>
2014-02-22 22:39:21 +0100

    Rearrange of assets folder

--- Zeega.Web/App_Start/BundleConfig.cs
+++ Zeega.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,24 @@ namespace Zeega.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/assets/js/jquery").Include(
+                        "~/assets/js/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/assets/js/jqueryval").Include(
+                        "~/assets/js/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/assets/js/modernizr").Include(
+                        "~/assets/js/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/assets/js/bootstrap").Include(
+                      "~/assets/js/bootstrap.js",
+                      "~/assets/js/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/style.css"));
+            bundles.Add(new StyleBundle("~/assets/css").Include(
+                      "~/assets/css/bootstrap.css",
+                      "~/assets/css/style.css"));
         }
     }
 }
--- Zeega.Web/assets/js/_references.js
+++ Zeega.Web/assets/js/_references.js
@@ -1,7 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="modernizr-2.7.1.js" />
 /// <reference path="jquery-2.1.0.js" />
-/// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
