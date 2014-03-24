ab80e813d388e67307d45a53971f334a898216de
Zeljko Tepsic <ztepsic@gmail.com>
2014-03-24 22:27:42 +0100

    Added backoffice web project

---
+++ Zeega.BackOffice.Web/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="modernizr-2.7.2.js" />
+/// <reference path="jquery-2.1.0.js" />
+/// <reference path="respond.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="respond.matchmedia.addlistener.js" />
---
+++ Zeega.BackOffice.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zeega.BackOffice.Web.Startup))]
+namespace Zeega.BackOffice.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
