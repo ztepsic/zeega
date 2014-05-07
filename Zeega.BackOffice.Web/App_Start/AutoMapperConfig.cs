24767eaaaea19ff4eafebcef6ae6d437c7756653
Zeljko Tepsic <ztepsic@gmail.com>
2014-05-07 20:35:27 +0200

    Started with backoffice

---
+++ Zeega.BackOffice.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Zeega.BackOffice.Web.App_Start {
+    /// <summary>
+    /// Auto mapper config class
+    /// </summary>
+    public static class AutoMapperConfig {
+
+        /// <summary>
+        /// Configures autom mapper with mappings
+        /// </summary>
+        public static void Configure() {
+
+        }
+    }
+}
--- Zeega.Web/Global.asax.cs
+++ Zeega.Web/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Threading;
 using System.Web.Configuration;
 using System.Web.Mvc;
