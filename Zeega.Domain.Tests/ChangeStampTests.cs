33a1c0016aeaac3fb614aa3cceae84e2f3fa0a84
Zeljko Tepsic <ztepsic@gmail.com>
2014-08-06 14:06:53 +0200

    Added write to console to tests for datetime debuging 5

--- Zeega.Domain.Tests/ChangeStampTests.cs
+++ Zeega.Domain.Tests/ChangeStampTests.cs
@@ -29,12 +29,12 @@ namespace Zeega.Domain.Tests {
             // Arrange
             DateTime utcTime = DateTime.UtcNow;
             ChangeStamp changeStamp = new ChangeStamp(utcTime);
-            Console.Error.WriteLine("UTC time: {0}", utcTime);
+            Console.WriteLine("UTC time: {0}", utcTime);
 
             // Act
             DateTime result = changeStamp.GetCreatedOn(TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"));
-            Console.Error.WriteLine("Central European Standard Time: {0}", result);
-            Console.Error.WriteLine("Daylight saving time: {0}", result.IsDaylightSavingTime());
+            Console.WriteLine("Central European Standard Time: {0}", result);
+            Console.WriteLine("Daylight saving time: {0}", result.IsDaylightSavingTime());
             
 
             // Assert
@@ -46,27 +46,30 @@ namespace Zeega.Domain.Tests {
 
         [Test]
         public void GetUpdatedOn_WithTimeZone_UpdatedOnInRequestedTimeZone() {
-            Console.WriteLine("Test console");
-            Debug.WriteLine("debug console");
-            Console.Error.WriteLine("Error console");
-            Trace.WriteLine("Trace console");
-
             // Arrange
             DateTime utcTime = DateTime.UtcNow;
             ChangeStamp changeStamp = new ChangeStamp(utcTime);
-            Console.Error.WriteLine("UTC time: {0}", utcTime);
+            Console.WriteLine("UTC time: {0}", utcTime);
 
             // Act
             DateTime result = changeStamp.GetUpdatedOn(TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"));
-            Console.Error.WriteLine("Central European Standard Time: {0}", result);
-            Console.Error.WriteLine("Daylight saving time: {0}", result.IsDaylightSavingTime());
+            Console.WriteLine("Result CEST: {0}", result);
+            Console.WriteLine("Result CEST is DST: {0}", result.IsDaylightSavingTime());
+            Console.WriteLine("Kind: {0}", result.Kind);
+            var cest = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+            var adjRules = cest.GetAdjustmentRules()[0];
+            Console.WriteLine("CEST support DST: {0}", cest.SupportsDaylightSavingTime);
+            Console.WriteLine("CEST DST delta: {0}", adjRules.DaylightDelta);
+            Console.WriteLine("CEST DST start-end: {0}.{1}-{2}.{3}", adjRules.DaylightTransitionStart.Day, adjRules.DaylightTransitionStart.Month, adjRules.DaylightTransitionEnd.Day, adjRules.DaylightTransitionEnd.Month);
+            Console.WriteLine("Local: {0}", TimeZoneInfo.Local);
+            Console.WriteLine("Local support DST: {0}", TimeZoneInfo.Local.SupportsDaylightSavingTime);
+            
 
 
             // Assert
             Assert.AreEqual(DateTimeKind.Unspecified, result.Kind);
             TimeSpan expectedTimeSpan = result.IsDaylightSavingTime() ? TimeSpan.FromHours(2) : TimeSpan.FromHours(1);
             Assert.AreEqual(expectedTimeSpan, result - utcTime);
-            Assert.Pass("test");
 
         }
 
